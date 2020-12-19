98bac8ac91ac4a350973464cd5e6442ce9de92d1
Alexa Carrington <acarring@wellesley.edu>
2020-12-19 15:35:51 -0500

    Added event listener to make pressing enter trigger button submit

--- app.js
+++ app.js
@@ -20,3 +20,12 @@ function checkAnswer() {
 }
 
 guessButton.addEventListener('click', checkAnswer);
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      guessButton.click();
+    }
+  });
